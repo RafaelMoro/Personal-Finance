fc342332de66bad4a97122a73e0e11c8580cc49d
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-05-30 20:34:12 -0500

    [ADD] - Starting webpack, babel, css, sass configuration

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/'
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@containers': path.resolve(__dirname, 'src/containers/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@actions': path.resolve(__dirname, 'src/redux/actions/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+            { loader: 'html-loader' }
+        ]
+      },
+      {
+        test: /\.(scss|css)$/i,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+        new CssMinimizerPlugin(),
+        new TerserPlugin()
+    ]
+  }
+}
