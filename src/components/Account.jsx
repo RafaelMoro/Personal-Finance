672bd5c190d5b818fd57a4eb49b459426bd89fef
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-06-12 10:37:13 -0500

    [ADD] - Delete and edit button were added to each account

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Paper, Typography, Grid } from '@mui/material'
+import { Paper, Typography, Grid, IconButton } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/DeleteOutlined'
+import EditIcon from '@mui/icons-material/EditOutlined';
 
 import { formatNumberToCurrency } from '../utils/formatNumberToCurrency'
 import '@styles/components/accounts.scss'
@@ -15,7 +17,16 @@ const Account = ({ account }) => {
     <>
       <Grid item xs={4}>
         <Paper elevation={2} sx={{ p: 2 }}>
+          <div className='card__title-buttons'>
             <Typography variant="h5" component="h4" >{account.name}</Typography>
+            <IconButton aria-label="edit">
+              <EditIcon />
+            </IconButton>
+            <IconButton aria-label="delete">
+              <DeleteIcon sx={{ color: 'red' }} />
+            </IconButton>
+          </div>
+            
             <div className='card__description'>
               <Typography color="text.secondary" >{account.type}</Typography>
               <Typography>$ {initialAmountFormated}</Typography>
